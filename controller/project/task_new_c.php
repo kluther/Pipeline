b71a401dea83de0c121b5481fd7ccb1db931ff52
Kurt Luther <kurt@kurtluther.com>
2011-09-02 04:59:41 +0000

    fixed permissions for create task
    git-svn-id: http://svn.research.cc.gatech.edu/pipeline/pipeline/trunk/pipeline2/demo@585 16799ac1-277d-452d-bde6-26ee59b221c8

--- controller/project/task_new_c.php
+++ controller/project/task_new_c.php
@@ -4,11 +4,15 @@ require_once("../../global.php");
 $slug = Filter::text($_GET['slug']);
 $project = Project::getProjectFromSlug($slug);
 
-// kick us out if slug invalid or not organizer
-if( ($project == null) ||
-	(!ProjectUser::isOrganizer(Session::getUserID(), $project->getID())) ) {
+// kick us out if slug invalid or not organizer/creator
+
+if($project == null) {
 	header('Location: '.Url::error());
 	exit();
+} elseif( (!ProjectUser::isOrganizer(Session::getUserID(), $project->getID())) &&
+	(!ProjectUser::isCreator(Session::getUserID(), $project->getID())) ) {
+	header('Location: '.Url::error());
+	exit();	
 }
 
 $soup = new Soup();
