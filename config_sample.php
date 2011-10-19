601f639c0132cb184e5f061f626edf781a42e980
Kurt Luther <kurt@kurtluther.com>
2011-10-19 21:29:47 +0000

    config sample updated
    git-svn-id: http://svn.research.cc.gatech.edu/pipeline/pipeline/trunk/pipeline2/demo@662 16799ac1-277d-452d-bde6-26ee59b221c8

--- config_sample.php
+++ config_sample.php
@@ -4,11 +4,11 @@
 	define('TEMPLATE_PATH', SYSTEM_PATH.'/views'); # where the views are - !NO CHANGE
 	define('CLASS_PATH', SYSTEM_PATH.'/model/classes'); # where the classes are - !NO CHANGE
 	define('UPLOAD_PATH', SYSTEM_PATH.'/upload'); # absolute path to where uploads are <- CHANGE THIS
-	define('THUMB_PATH', UPLOAD_PATH.'/thumb'); # absolute path to where thumbnails are <- CHANGE THIS
-	define('PREVIEW_PATH', UPLOAD_PATH.'/preview'); # absolute path to where previews are <- CHANGE THIS
-	define('USER_PICTURE_PATH', UPLOAD_PATH.'/user'); # absolute path to unedited user pictures
-	define('USER_PICTURE_LARGE_PATH', USER_PICTURE_PATH.'/large'); # absolute path to large user picture thumbnail
-	define('USER_PICTURE_SMALL_PATH', USER_PICTURE_PATH.'/small'); # absolute path to small user picture thumbnail
+	define('THUMB_PATH', UPLOAD_PATH.'/thumb'); # absolute path to where thumbnails are - !NO CHANGE
+	define('PREVIEW_PATH', UPLOAD_PATH.'/preview'); # absolute path to where previews are - !NO CHANGE
+	define('USER_PICTURE_PATH', UPLOAD_PATH.'/user'); # absolute path to unedited user pictures - !NO CHANGE
+	define('USER_PICTURE_LARGE_PATH', USER_PICTURE_PATH.'/large'); # absolute path to large user picture thumbnail - !NO CHANGE
+	define('USER_PICTURE_SMALL_PATH', USER_PICTURE_PATH.'/small'); # absolute path to small user picture thumbnail - !NO CHANGE
 	
 	define('BASE_URI', 'http://pipeline.cc.gatech.edu'); # base URI for this Pipeline instance <- CHANGE THIS
 	define('PIPELINE_NAME', 'Pipeline Demo'); # name of this Pipeline instance <- CHANGE THIS
